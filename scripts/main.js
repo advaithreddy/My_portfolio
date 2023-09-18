7521fce56fdc67179cd230f0085b1b508bb5ab57
Advaith Reddy <83079702+advaithreddy@users.noreply.github.com>
2023-09-18 15:47:15 +0530

    Push : Added copy button

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// Timer 
 var countDownDate = new Date("Sep 30, 2023 00:00:00").getTime();
 var x = setInterval(function(){
     var now = new Date().getTime();
@@ -14,3 +15,22 @@ var x = setInterval(function(){
     document.getElementById("seconds").innerHTML = seconds;
 
 },1000);
+
+// Signup api
+function copyToClipboard(button) {
+    const codeText = button.parentElement;
+    const range = document.createRange();
+    range.selectNode(codeText);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
+
+function showCopyButton(element) {
+    element.querySelector('.copy-button').style.display = 'inline-block';
+}
+
+function hideCopyButton(element) {
+    element.querySelector('.copy-button').style.display = 'none';
+}
+
